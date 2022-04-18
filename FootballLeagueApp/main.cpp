f3c4feb056d0f37c3fb2f8a9b7878e20cffab29c
James Connolly <jconnolly94@me.com>
2022-04-18 18:35:04 +0100

    Initial Commit

---
+++ FootballLeagueApp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FootballLeagueApp
+//
+//  Created by James Connolly on 18/04/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
